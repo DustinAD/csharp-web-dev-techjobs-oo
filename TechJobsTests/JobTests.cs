2b2ac6177977a86c64eda706c7067aa330c2cd10
DustinAD <dustind_daydugan@yahoo.com>
2020-10-28 00:30:19 -0500

    new TechJobsTests

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TechJobsTests
 {
     [TestClass]
-    public class UnitTest1
+    public class JobTests
     {
         [TestMethod]
         public void TestMethod1()
